45db97c03d44ed3529f4ed01591906d3ef78bb25
KambaAbi <Kambaa@users.noreply.github.com>
2023-11-11 20:41:21 +0300

    Update çalıştır.bat

--- çalıştır.bat
+++ çalıştır.bat
@@ -1,9 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
-REM BURAYA ÇIKARDIĞINIZ KLASÖRÜN YOLUNU YAZINIZ
+
+REM cwd bilgisi olarak klasörün bulunduğu yolu yazın
 SET "cwd=Z:\ProgramFiles\ytdl-by-kambaabi"
-REM BURAYA VİDEOLARI İNME KLASÖRÜN YOLUNU YAZINIZ
+REM export_dir bilgisi olarak indirilen videoların nereye kaydedileceğini yazın
 SET "export_dir=%userprofile%\Videos\"
+REM browsername bilgisi olarak oturum açılması gerekecek siteler için (ör. instagram) oturum açılan tarayıcı adını yazın. 
+REM brave, chrome, chromium, edge, firefox, opera, safari, vivaldifirefox gibi
+SET "browsername=firefox"
 
 SET filename_ytdlp="%cwd%\yt-dlp.exe"
 SET filename_ffmpeg="%cwd%\ffmpeg.exe"
@@ -55,27 +59,44 @@ rem /T 3 saniye bekledikten sonra
 rem /D 1 ile varsayılan olarak 1'i otomatik seçmesini ayarladım.
 choice /c 12 /n /D 1 /T 3 /m "1 ya da 2 Girin: "
 
+
+
 if errorlevel 2 (
-  set "format=mp3"
-  goto mp3download
+ set "format=mp3"
+rem  goto mp3download
 ) else if errorlevel 1 (
-  set "format=mp4"
-  goto mp4download
+ set "format=mp4"
+rem  goto mp4download
 ) else (
    echo "varsayilan secildi"
-   goto mp4download
+   set "format=mp4"
+rem   goto mp4download
 )
+
+echo "Oturum Acilmasi Gereken Yerler Icin"
+echo "Firefox Cerezlerini(cookies) Kullan:"
+echo "y(EVET) n(HAYIR)"
+choice /c yn /n /D n /T 2 /m "y(evet) ya da n(hayir) girin: "
+if errorlevel 2 (
+  echo "Cerezleri(cookie) kullanmayacak!"
+  set "cookieArg="
+) else (
+  echo "Cerezleri(cookie) kullanacak"
+  set "cookieArg=--cookies-from-browser %browsername%"
+)
+
+goto %format%download
 
 goto endofoperation
 
 :mp4download
 echo "MP4 olarak Indiriliyor:" !url!
-call  yt-dlp.exe !url! -f "bv*+ba/b" --restrict-filenames -P %export_dir% -o "%%%%(title)s%%%%(resolution)s.%%%%(ext)s" --ffmpeg-location %filename_ffmpeg% --merge-output-format "mp4" --recode-video "mp4" 
+call  yt-dlp.exe !url! -f "bv*+ba/b" %cookieArg%  --restrict-filenames -P %export_dir% -o "%%%%(title)s%%%%(resolution)s.%%%%(ext)s" --ffmpeg-location %filename_ffmpeg% --merge-output-format "mp4" --recode-video "mp4" 
 goto :endofoperation
 
 :mp3download
 echo "MP3 olarak Indiriliyor:" !url!
-call yt-dlp.exe !url! -P %export_dir% -o "%%%%(title)s.%%%%(ext)s" --restrict-filenames -f "(bv*+ba/b)" --ffmpeg-location %filename_ffmpeg% --recode-video "mp3"
+call yt-dlp.exe !url! %cookieArg% -P %export_dir% -o "%%%%(title)s.%%%%(ext)s" --restrict-filenames -f "(bv*+ba/b)" --ffmpeg-location %filename_ffmpeg% --recode-video "mp3"
 goto endofoperation
 
 
